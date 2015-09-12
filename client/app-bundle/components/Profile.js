88157fe41311407a5ae7c91680ff8c3cc683c7a9
Dan Corman <dan.corman@gmail.com>
2015-09-12 16:29:55 -0500

    establishing basic profile seperation

--- client/app-bundle/components/Profile.js
+++ client/app-bundle/components/Profile.js
@@ -13,14 +13,17 @@ Profile.controller = function () {
 
 Profile.view = function (ctrl, options) {
     console.log(options)
-  return m('.User', [
-    ['div', 
-        m('img[src=' + options.user.avatarUrl + ']'),
-        m('h4', options.user.username),
-        m('h5', options.user.name),
-    ],
-    m.component(Feed, { squashes: options.squashes}),
-    m('h5', options.squashes.title),
-    m('p', options.squashes.description)
-  ])
+  return m('.container', [ 
+    m('.User', [
+            m('img[src=' + options.user.avatarUrl + ']'),
+            m('span', 'Username: ' + options.user.username),
+            m('br'),
+            m('span', 'Name: ' + options.user.name),
+        ]),
+        m('.userSquashes', [
+            m.component(Feed, { squashes: options.squashes}),
+            m('h5', options.squashes.title),
+            m('p', options.squashes.description)
+        ])
+    ])
 }
