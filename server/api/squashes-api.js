f85536497df383345415c97bbbc4813f9a8b21e0
Dan Corman <dan.corman@gmail.com>
2015-09-12 12:55:56 -0500

    adding squashes model

--- server/api/squashes-api.js
+++ server/api/squashes-api.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var Users = require('../models/Users.js');
+var Users = require('../models/Squashes.js');
 
 var router = module.exports = express.Router();
 
 //gets all users with user information
 router.get('/', function(req, res){
-    res.send(Users.getUser(1));
+    res.send(Users.getSquashes(1));
 });
 
 router.post('/', function(req, res){
@@ -15,6 +15,6 @@ router.post('/', function(req, res){
 
 //get all user squahes
 router.get('/:id/', function(req, res){
-    res.send(Users.getUser(1));
+    res.send(Users.getSquashes(1));
 })
 
---
+++ server/models/Squashes.js
@@ -0,0 +1,10 @@
+var Promise = require('bluebird');
+var Squashes = module.exports;
+
+
+var Squashes =  ['drank', 'drunk', 'deployed', 'got', 'developed', 'built', 'invented', 'experienced', 'fought off', 'hardened', 'enjoyed', 'developed', 'consumed', 'debunked', 'drugged', 'doped', 'made', 'wrote', 'saw'];
+
+module.exports.getSquashes = function () {
+    return Squashes;
+}
+
