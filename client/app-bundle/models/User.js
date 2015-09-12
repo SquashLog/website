e2a5d2179b3a3ca42ff78b85e4bcecdf979da645
Karen Lewis <karmakettle@gmail.com>
2015-09-12 12:15:01 -0500

    refactored and added find method

--- client/app-bundle/models/User.js
+++ client/app-bundle/models/User.js
@@ -1,6 +1,9 @@
 var m = require('mithril')
 
-module.exports = function(attrs) {
+var User = module.exports;
+
+
+User.vm = function(attrs) {
   var attrs = attrs || ''
   return {
     name: m.prop(attrs.name),
@@ -8,4 +11,9 @@ module.exports = function(attrs) {
     avatar_url: m.prop(attrs.avatar_url),
     email: m.prop(attrs.email)
   }
+}
+
+User.find = function (id) {
+  // TODO karmakettle: this api url might change later--confirm
+  return m.request({ method: 'GET', url: '/users/' + id });
 }
