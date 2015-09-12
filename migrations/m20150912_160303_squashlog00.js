d472b4bf02158e49c27326d94753d5a46b5768c7
quikstrike <quikstrik3@gmail.com>
2015-09-12 11:34:09 -0500

    User migration file completed

---
+++ migrations/m20150912_160303_squashlog00.js
@@ -0,0 +1,63 @@
+"use strict";
+exports.name = "squashlog00";
+
+exports.up = function (db) {
+  // @todo implementation
+  db.class.create('V', 'User')
+  .then(function (User) {
+    console.log('Created class: ' + User);
+  });
+
+  User.property.create({
+    name: 'name',
+    type: 'String'
+  }, {
+    name: 'username',
+    type: 'String'
+  },{
+  	name: 'avatar_url',
+  	type: 'string'
+  },{
+  	name: 'email',
+  	type: 'string'
+  },{
+  	name: 'updatedAt'
+  	type: 'Datetime'
+  },{
+  	name: 'createdAt'
+  	type: 'Datetime'
+  }
+  )
+  .then(function () {
+    console.log('Property created.')
+  });
+};
+
+exports.down = function (db) {
+  // @todo implementation
+  User.property.drop('name')
+  .then(function () {
+    console.log('Property deleted.');
+  });
+  User.property.drop('username')
+  .then(function () {
+    console.log('Property deleted.');
+  });
+  User.property.drop('avatar_url')
+  .then(function () {
+    console.log('Property deleted.');
+  });
+  User.property.drop('email')
+  .then(function () {
+    console.log('Property deleted.');
+  });
+  User.property.drop('updatedAt')
+  .then(function () {
+    console.log('Property deleted.');
+  });
+  User.property.drop('createdAt')
+  .then(function () {
+    console.log('Property deleted.');
+  });
+};
+
