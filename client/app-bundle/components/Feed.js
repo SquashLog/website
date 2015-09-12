c6cfb86cb6c8f4708f355bda96691a095e6898e2
Karen Lewis <karmakettle@gmail.com>
2015-09-12 14:18:02 -0500

    changed method name

--- client/app-bundle/components/Feed.js
+++ client/app-bundle/components/Feed.js
@@ -3,7 +3,7 @@ var Squash = require('../models/Squash')
 
 exports.controller = function (options) {
   var ctrl = this;
-  ctrl.squashes = Squash.getAll();
+  ctrl.squashes = Squash.all();
 }
 
 exports.view = function (ctrl, options) {
