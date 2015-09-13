177dbf361c51fb5cdfcf5df3dc86fe995d848aa3
Dan Corman <dan.corman@gmail.com>
2015-09-12 20:56:49 -0500

    working github oAuth

---
+++ server/Auth.js
@@ -0,0 +1,33 @@
+var passport = require('passport')
+var GitHubStrategy = require('passport-github').Strategy
+var Promise    = require('bluebird')
+
+
+
+exports.mount = function (app, host) {
+
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:4000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
+
+app.use(passport.initialize())
+  app.use(passport.session())
+
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+};
