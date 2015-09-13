82d3c561fb84c351c2d829efb2cd7d78febfec29
Karen Lewis <karmakettle@gmail.com>
2015-09-12 19:44:19 -0500

    added more test users and modified find method

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,21 +1,23 @@
 var Promise = require('bluebird');
 var Users = module.exports;
 
-var testUser = {uid: 1, name: 'Beavis', username: 'MeyerSauce21', password: 'ChickenShack22', followers: ['eric','stan','kenny','kyle'], squashes : [] }
+var testUsers = [{uid: 1, name: 'Beavis', username: 'MeyerSauce21', password: 'ChickenShack22', followers: ['eric','stan','kenny','kyle'], squashes : [] },
+    {uid: 1, name: 'Butthead', username: 'eric122', password: 'ChickenShack22', followers: ['eric','stan','kenny','kyle'], squashes : [] },
+    {uid: 1, name: 'Bungholio', username: 'mrKITTTYYYY!', password: 'ChickenShack22', followers: ['eric','stan','kenny','kyle'], squashes : [] }
+  ]
 
 
 Users.find = function (username) {
-    if (testUser.username === username) return testUser;
-    else return {error: 'No such user, yet!'};
+    return testUsers.filter(function(user) { return user.username === username; })[0];
 }
 
 Users.followers = function (username) {
-    if (testUser.username === username) return {followers: testUser.followers};
+    if (testUsers.username === username) return {followers: testUser.followers};
     else return 'No such user, yet!'
 }
 
 Users.squashes = function (username) {
-    if (testUser.username === username) return {squashes: testUser.squashes};
+    if (testUsers.username === username) return {squashes: testUser.squashes};
     else return 'No such user, yet!'
 }
 
