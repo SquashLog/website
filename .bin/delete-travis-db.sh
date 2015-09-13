af7f22cc958b3595b7f1baca7f305eb18e4a8780
daniel-jansen <daniel.jansen88@gmail.com>
2015-09-13 15:59:24 -0500

    Created shell script for travis/orientdb compatibility

---
+++ .bin/delete-travis-db.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#set DB_NAME to branch name
+DB_NAME=$TRAVIS_BRANCH
+
+SUB_DIR='$DB_NAME/plocal/graph'
+
+TEST_PATH=$DB_URL$SUB_DIR
+
+#initialize AUTH_TOKEN
+AUTH_TOKEN=$(echo -n 'DB_USERNAME:DB_PASSWORD' | openssl base64)
+
+#create new db to remote ip
+curl -X DELETE -H "Authorization: Basic $AUTH_TOKEN" $TEST_PATH && echo ' done.'
