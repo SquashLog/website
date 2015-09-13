9b5432ab3b8dd9baae0b640c405c2ec1e25f2afe
daniel-jansen <daniel.jansen88@gmail.com>
2015-09-13 13:48:44 -0500

    Altered setup.sh to include migrations

--- .bin/setup.sh
+++ .bin/setup.sh
@@ -6,6 +6,13 @@ echo "Initializing script..."
 #check if user has java runtime installed
 type java >/dev/null 2>&1 || { echo >&2 "Please install java and try running this script again.  Aborting."; exit 1;}
 
+#check install node 4.0
+#if node --version !== v4.0.0
+#  echo "Please install latest node version."
+#  echo " "
+#  echo "Run update-node.sh to update to node v4.0.0."
+#fi
+
 clear
 
 brew install wget
@@ -18,6 +25,7 @@ rm -rf orientdb-community-2.1.2
 rm orientdb.tar.gz
 
 # chmod every .sh file to be executable
+chmod 755 ./.bin/db-reset.sh
 chmod 755 ./orientdb/bin/*.sh
 chmod -R 777 ./orientdb/config
 
@@ -25,7 +33,7 @@ cd ./orientdb/bin/
 
 echo development | ./server.sh &
 pid=$!
-sleep 20
+sleep 15
 
 AUTH_TOKEN=$(echo -n 'root:development' | openssl base64)
 echo "AUTH: $AUTH_TOKEN"
@@ -37,8 +45,15 @@ echo -n 'Creating test database...'
 curl -X POST -H "Authorization: Basic $AUTH_TOKEN" 'http://localhost:2480/database/squash_test/plocal/graph' && echo ' done.'
 
 kill ${pid}
+
+sleep 5
+
+cd ../../
 npm install
-# npm run migrate
+
+echo "Running database migrations..."
+
+npm run migrate
 
 echo "Setup Completed"
 echo "DEV OPS YO"
