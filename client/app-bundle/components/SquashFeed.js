a42795646649ca9b75982567dd6f6a1da11e112b
Gilbert <gilbertbgarza@gmail.com>
2015-10-27 10:32:07 -0500

    Minor refactor

--- client/app-bundle/components/SquashFeed.js
+++ client/app-bundle/components/SquashFeed.js
@@ -5,21 +5,23 @@ exports.controller = function (options) {}
 
 exports.view = function (ctrl, options) {
     return m('.squash-feed', [
-      options.squashes.map(function(squash) {
-        return m('.squash', [
-          m('.mti', { 'data-tag': squash.tags[0] }),
-          m('h2.title', [
-            m('span.main-tag', '['+ squash.tags[0]  +'] '),
-            m('a', {
-              href: '/' + squash.user.username + '/squash/' + squash.id,
-              config: m.route
-            }, squash.title)
-          ]),
-
-          squash.content ? [
-            m('.content', squash.content)
-          ] : null,
-        ])
-      })
+      options.squashes.map(squashView)
     ])
+}
+
+function squashView (squash) {
+  return m('.squash', [
+
+    m('.mti', { 'data-tag': squash.tags[0] }),
+
+    m('h2.title', [
+      m('span.main-tag', '['+ squash.tags[0]  +'] '),
+      m('a', {
+        href: `/${squash.user.username}/squash/${squash.id}`,
+        config: m.route
+      }, squash.title)
+    ]),
+
+    m('.content', squash.content)
+  ])
 }
